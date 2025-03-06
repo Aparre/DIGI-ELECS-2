159219730c99bdd7a9d4f2db01cc7e8f3e32a976
MacApe <87509600+Aparre@users.noreply.github.com>
2025-03-07 04:54:12 +0800

    Update 5.3_notTACSW.ino

--- 5.3_notTACSW.ino
+++ 5.3_notTACSW.ino
@@ -1,17 +1,17 @@
-int name = 2; //digital
-int nameLed = 3; //digital pin
+int ulp = 2; //digital
+int ulpLed = 3; //digital pin
 void setup() {
-  pinMode(name, INPUT);
-  pinMode(nameLed, OUTPUT);
+  pinMode(ulp, INPUT);
+  pinMode(ulpLed, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  int SwLevel = digitalRead(name);
+  int SwLevel = digitalRead(ulp);
   if (SwLevel == LOW) 
-    digitalWrite(nameLed, LOW);
+    digitalWrite(ulpLed, LOW);
   else
-    digitalWrite(nameLed, HIGH);
+    digitalWrite(ulpLed, HIGH);
   Serial.print("Switch: ");
   Serial.println(SwLevel);
   delay(30);
