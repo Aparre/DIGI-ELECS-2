fed91c467c499c663dc0a5c196216f691aa0d103
MacApe <87509600+Aparre@users.noreply.github.com>
2025-03-07 04:50:39 +0800

    Update 5.4_ANDTACsw.ino

--- 5.4_ANDTACsw.ino
+++ 5.4_ANDTACsw.ino
@@ -1,20 +1,20 @@
-int name = 2; //digital
-int name2 = 3; // digital pin
-int nameLed = 4; //digital pin
+int ulp = 2; //digital
+int ulp2 = 3; // digital pin
+int ulpLed = 4; //digital pin
 void setup() {
-  pinMode(name, INPUT);
-  pinMode(name2, INPUT);
-  pinMode(nameLed, OUTPUT);
+  pinMode(ulp, INPUT);
+  pinMode(ulp2, INPUT);
+  pinMode(ulpLed, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  int SwLevel = digitalRead(name);
-  int Sw2Level = digitalRead(name2);
+  int SwLevel = digitalRead(ulp);
+  int Sw2Level = digitalRead(ulp2);
   if (SwLevel == LOW && Sw2Level == LOW)
-    digitalWrite(nameLed, HIGH);
+    digitalWrite(ulpLed, HIGH);
   else
-    digitalWrite(nameLed, LOW);
+    digitalWrite(ulpLed, LOW);
   Serial.print("Switch: ");
   Serial.println(SwLevel);
 }
