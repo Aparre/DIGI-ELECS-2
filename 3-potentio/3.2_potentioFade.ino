c645e887e05b2075e0fe77892553fc587a9d950f
MacApe <87509600+Aparre@users.noreply.github.com>
2025-04-02 01:51:15 +0800

    Update 3.2_potentioFade.ino

--- 3-potentio/3.2_potentioFade.ino
+++ 3-potentio/3.2_potentioFade.ino
@@ -1,1 +1,24 @@
+int nat = A0;                   //store analog pin A0 to variable nat
+int natled = 3;                 //set digital pin 3 for led
+//choose digital pin with ~ or capable for sending PWM signals
+void setup() {
+  pinMode(nat, INPUT);          //initializing nat in input mode
+  pinMode(natled, OUTPUT);      //initializing nat in output mode
+  Serial.begin(9600);           //initializing serial monitor with 9600 baud
+}
 
+void loop() {
+  int natIn = analogRead(nat);  //read and store the nat value in natIn
+  /*  map(variable, min1, max1, min2, max2);
+      variable     = refers to the value measured to be mapped
+      min1 , max1  = refers to the minimum and maximum value of the actual measured value
+      min2 , max2  = refers to the minimum and maximum value of the value to be outptted by the map
+
+      the map() function will correlate the values from the reference(actual) to the outputted, not only the
+      range(min2, max2) are the outputted values and exceeding the range(min1, max1) results to exceeding the values for 
+  */
+  int natIN = map(natIn, 0, 1023, 0, 255);
+  digitalWrite(natled, natIN);  //write the mapped value of natIN to the LED
+  Serial.println(natIN);        //print out the natIN value in the serial monitor, which indicates the brightness of the LED
+  delay(30);                    //wait for 30 milliseconds to run the code again
+}
