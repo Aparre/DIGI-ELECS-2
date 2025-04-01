b529753100d89e3fe206a4cc0b08a3992af7a20e
MacApe <87509600+Aparre@users.noreply.github.com>
2025-04-02 00:50:06 +0800

    Update 2.1_fade.ino

--- 2-led/2.1_fade.ino
+++ 2-led/2.1_fade.ino
@@ -1,19 +1,22 @@
-int nat = 9;        
-int brightness = 0;  
-int fadeAmount = 5; 
+int nat = 9;                  //digital pin 9 for led
+int brightness = 0;           //varible for storing led brightness
+int fadeAmount = 5;           // variable for incrementation of brightness value
 
 void setup() {
-  pinMode(nat, OUTPUT);
-  Serial.begin(9600);
+  pinMode(nat, OUTPUT);       //setting the pin 9 to be output
+  Serial.begin(9600);         // initializing serial monitor with 9600 baud
 }
 
 void loop() {
-  analogWrite(nat, brightness);
-  brightness = brightness + fadeAmount;
+  //at first execution the brightness value is 0, then later be adjusted
+  analogWrite(nat, brightness); 
+  brightness = brightness + fadeAmount;   //computing the next value of brightness
+
+  //condition for when the brightness value excceds 255 the fadeamount value is negated resulting in decreasing value of brightness
   if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
+    fadeAmount = -fadeAmount; 
   }
-  int natlevel = map(brightness, 0, 255, 0, 255);
-  Serial.println(natlevel);
-  delay(30);
+  Serial.println(brightness);             //display the value of brightness 
+  delay(30);                              // wait for 30 millisecond to run the code again
+  // at this point the brightness value is being adjusted with respect to the computation in line 13
 }
